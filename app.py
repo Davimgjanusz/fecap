274bc15e262349eb85500a57843fef8c3638bc7a
Davimgjanusz <davi.mgarciaj@gmail.com>
2025-06-11 12:23:29 -0300

    1.0

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cursos')
+def cursos():
+    return render_template('cursos.html')
+
+@app.route('/portal')
+def portal():
+    return render_template('portal.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
